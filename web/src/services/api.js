df808c6b96b095338e27874e5e813b7d976771ec
João Victor dos Santos <jvictorsantos852@gmail.com>
2019-10-31 10:14:17 -0300

    Fix environment variables for web, for deployment on digitalocean

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development'
+    process.env.REACT_APP_ENV === 'development'
       ? 'http://localhost:3333'
       : process.env.REACT_APP_API,
 });
